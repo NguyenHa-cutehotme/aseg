d2f756290e3a943e923cf4513a5be9809c32e521
hadtt <hadtt@segu.vn>
2019-09-20 18:29:58 +0700

    file chooser

--- src/demoase.java
+++ src/demoase.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class demoase {
     private JButton button1;
@@ -12,13 +14,30 @@ public class demoase {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JFileChooser chooser = new JFileChooser();
-                FileNameExtensionFilter filter = new FileNameExtensionFilter(
-                        "JPG & GIF Images", "jpg", "gif");
-                chooser.setFileFilter(filter);
                 int returnVal = chooser.showOpenDialog(null);
                 if(returnVal == JFileChooser.APPROVE_OPTION) {
+                    String t = chooser.getSelectedFile().getAbsolutePath();
+                    String replaceString = t.replace("\\", "/");
                     System.out.println("You chose to open this file: " +
-                            chooser.getSelectedFile().getAbsolutePath());
+                           replaceString);
+                    FileInputStream fin = null;
+                    try {
+                        fin = new FileInputStream(replaceString);
+                        int i = 0;
+                        while ((i = fin.read()) != -1) {
+                            System.out.print((char) i);
+                        }
+                        fin.close();
+                    } catch (Exception e1) {
+                        System.out.println(e1);
+                    } finally {
+                        try {
+                            fin.close();
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+
                 }
             }
 
